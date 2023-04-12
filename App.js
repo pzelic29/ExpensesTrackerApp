8dcf10a84d0cb5f82764fbc5d4c4e3e1d41b9467
pzelic29 <pzelic29@gmail.com>
2023-04-12 21:47:49 +0200

    Adding navigation

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
-import {StatusBar} from 'expo-status-bar'
-import {NavigationContainer} from '@react-navigation/native'
-import AllExpenses from './screens/AllExpenses'
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import AllExpenses from "./screens/AllExpenses";
+import ManageExpense from "./screens/ManageExpenses";
+import RecentExpenses from "./screens/RecentExpenses";
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpensesOverview(){
+  return <BottomTabs.Navigator>
+    <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses} />
+    <BottomTabs.Screen name="AllExpenses" component={AllExpenses} />
+  </BottomTabs.Navigator>
+}
+
 export default function App() {
   return (
     <>
-    <StatusBar style="auto"/>
-    <NavigationContainer>
-    <AllExpenses />
-    </NavigationContainer>
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator>
+        <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} />
+          <Stack.Screen name="ManageExpense" component={ManageExpense} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
-
